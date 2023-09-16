81e9963bea48a28f805637d62b639184852ac8e1
arseniismpx <56837173+arseniismpx@users.noreply.github.com>
2023-09-16 23:07:10 +0200

    add navigation

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import Person from './components/Person';
 import Button from './components/Button';
 import { useState } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,18 +26,20 @@ export default function App() {
   });
 
   return (
-    <View style={styles.container}>
-      <Text>This is the Birthday comparator app</Text>
-      <Person
-        name={person.name}
-        imageUrl='https://cdn-icons-png.flaticon.com/512/2641/2641391.png'
-      />
-      <Button style={styles.addButton}
-        title='Add new person'
-        onPress={addPerson}
-        // imageUrl='https://cdn.icon-icons.com/icons2/1760/PNG/512/4105962-add-expand-plus_113920.png'
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>This is the Birthday comparator app</Text>
+        <Person
+          name={person.name}
+          imageUrl='https://cdn-icons-png.flaticon.com/512/2641/2641391.png'
+          />
+        <Button style={styles.addButton}
+          title='Add new person'
+          onPress={addPerson}
+          // imageUrl='https://cdn.icon-icons.com/icons2/1760/PNG/512/4105962-add-expand-plus_113920.png'
+          />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
